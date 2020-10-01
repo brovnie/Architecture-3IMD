2a4d4c82caac123407cca2b3f3442a84cea96563
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-10-01 14:30:54 +0200

    Created basic .NET application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Architecture_3IMD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
